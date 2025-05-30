5079cfc69619e9cce2d5a2d4872afe884067eec9
Harish Nigam <harishnigam21@gmail.com>
2025-05-30 22:26:59 +0530

    update socket

--- app/public/js/socket.io.js
+++ app/public/js/socket.io.js
@@ -1,4 +1,4 @@
-import { io } from 'socket.io-client';
+import { io } from 'https://cdn.socket.io/4.7.5/socket.io.min.js';
 const socket = io("https://vc-front-six.vercel.app");
 
 socket.on("connect", () => {
